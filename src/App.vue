a86173c88257069053b5d5a160227f27a9063488
CastellucciLuca <castellucciluca98@gmail.com>
2023-01-13 15:10:40 +0100

    imported API

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,36 @@
 <script >
+import {store} from './store.js';
+import axios from 'axios';
 import AppHeader from './components/AppHeader.vue';
 import AppMain from './components/AppMain.vue';
 export default {
   name: 'App',
-  components: { AppHeader, AppMain }
+  components: { AppHeader, AppMain },
+  data() {
+    return {
+      store,
+      apiUrl: 'https://api.themoviedb.org/3/search/movie?'
+    }
+  },
+  methods: {
+    getFilmApi(importUrl) {
+      axios.get(importUrl, {
+        params: {
+          api_key: 'afbf158776bc993869bf790f7b9b71de',
+          language: 'it-IT',
+          query: 'a'
+        }
+      })
+        .then(function (response) {
+          console.log(response.data);
+        })
+        .catch(function (error) {
+          console.log(error);
+        })
+    },
+  },
+  created() {
+    this.getFilmApi(this.apiUrl)
+  },
 }
 </script>
-
-<template>
-
-</template>
-
-<style>
-</style>
